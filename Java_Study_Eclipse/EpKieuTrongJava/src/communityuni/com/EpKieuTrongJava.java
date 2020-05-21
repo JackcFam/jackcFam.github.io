d100f77197535b3cb6aafc09179c5670e729961a
Nhat Pham <phamnhat05111997@gmail.com>
2020-05-21 16:52:15 +0700

    update bai tap java

---
+++ Java_Study_Eclipse/EpKieuTrongJava/src/communityuni/com/EpKieuTrongJava.java
@@ -0,0 +1,16 @@
+package communityuni.com;
+
+public class EpKieuTrongJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 1/2;
+		double b = 1.0/2;
+		System.out.print(a+"\n");
+		System.out.print(b);
+		System.out.println("\n"+(double)1/2);
+		double x = (double)10/3;
+		System.out.println(x);
+	}
+
+}
---
+++ Java_Study_Eclipse/KhaiBaoBien/src/communityuni/com/KhaiBaoBien.java
@@ -0,0 +1,12 @@
+package communityuni.com;
+
+public class KhaiBaoBien {
+	public static void main(String[] args) {
+		System.out.println("===============");
+		float t2 = 5.5f;
+		System.out.println("t2\t"+t2);
+		final double PI = 3.14;
+		System.out.println("\t"+PI);
+		
+	}
+}
